c6080be152e77a30176535ecaf56b4b3fe27820b
EC2 Default User <ec2-user@ip-172-31-20-4.ec2.internal>
2021-08-10 12:14:04 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
   end
 
   def index
@@ -34,16 +36,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path, notice: "You have updated user successfully."
     else
-      render "show"
+      render 'edit'
     end
   end
+  
+  
 
   private
+  
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
